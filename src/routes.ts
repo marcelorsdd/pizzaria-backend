8068b17c9737dd8a42777162637199ff6fbbde7a
Marcelo Silva <marcelorsdd@gmail.com>
2023-06-11 18:02:46 -0300

    feat: Create middleware for verify if user is authentication

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
+import { DetailUserController } from './controllers/user/DetailUserController';
+import { isAuthentication } from './middlewares/isAuthentication';
 
 const router = Router();
 
 // USERS
 router.post('/users', new CreateUserController().handle);
 router.post('/session', new AuthUserController().handle);
+router.get('/me', isAuthentication, new DetailUserController().handle);
 
 export { router };
