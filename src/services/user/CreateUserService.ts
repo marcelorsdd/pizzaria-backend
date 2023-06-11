451cd7765cbb7019dbd689e162d82eb3cca13bff
Marcelo Silva <marcelorsdd@gmail.com>
2023-06-11 16:43:54 -0300

    feat(user): Create execute function for save user in database

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,3 +1,4 @@
+import prisma from '../../prisma';
 interface ICreateUserServiceProps {
   name: string;
   email: string;
@@ -5,7 +6,39 @@ interface ICreateUserServiceProps {
 }
 
 class CreateUserService {
-  async execute(data: ICreateUserServiceProps) {}
+  async execute(data: ICreateUserServiceProps) {
+    await this.verifyUserExist(data.email);
+
+    if (!data.email) {
+      throw new Error('Email incorrect');
+    }
+
+    return await prisma.user.create({
+      data: {
+        name: data.name,
+        email: data.email,
+        password: data.password,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        created_at: true,
+      },
+    });
+  }
+
+  async verifyUserExist(email: string) {
+    const user = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (user) {
+      throw new Error('User already exists');
+    }
+  }
 }
 
 export { CreateUserService };
